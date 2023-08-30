4878c4d35695787a3277be5e188fbb207ca48b05
Adam Price <komidore64@gmail.com>
2023-08-30 16:52:45 -0400

    wip
    kkga skeleton

--- keyboards/ferris/keymaps/komidore64/keymap.c
+++ keyboards/ferris/keymaps/komidore64/keymap.c
@@ -17,10 +17,28 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ALPHA] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R,   KC_T,    KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F,   KC_G,    KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN,
-        KC_Z, KC_X, KC_C, KC_V,   KC_B,    KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                          KC_TAB, KC_LSFT, KC_SPC, KC_ENT
+        KC_Q, KC_W, KC_E, KC_R,       KC_T,    KC_Y,   KC_U,       KC_I,    KC_O,   KC_P,
+        KC_A, KC_S, KC_D, KC_F,       KC_G,    KC_H,   KC_J,       KC_K,    KC_L,   KC_SCLN,
+        KC_Z, KC_X, KC_C, KC_V,       KC_B,    KC_N,   KC_M,       KC_COMM, KC_DOT, KC_SLSH,
+                          MO(EXTEND), KC_LSFT, KC_SPC, MO(SYMBOL)
+    ),
+    [EXTEND] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,
+                             KC_TRNS, KC_NO, KC_NO, MO(NUMBER)
+    ),
+    [SYMBOL] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+                             MO(NUMBER), KC_NO, KC_NO, KC_TRNS
+    ),
+    [NUMBER] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+                             KC_TRNS, KC_NO, KC_NO, KC_TRNS
     ),
 };
 // clang-format on
