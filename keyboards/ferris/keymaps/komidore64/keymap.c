569b6a8f372aa1dbd9ba5684a165639fa4205136
Adam Price <komidore64@gmail.com>
2023-08-30 15:59:40 -0400

    wip

--- keyboards/ferris/keymaps/komidore64/keymap.c
+++ keyboards/ferris/keymaps/komidore64/keymap.c
@@ -2,24 +2,25 @@
 
 // #include "version.h"
 
+// reference / inspiration: https://keymapdb.com/keymaps/kkga/
+//
+// TODO: see if it's possible to include a mouse layer after using kkga for a
+// bit.
+
 enum layers {
-    ALPHAS,
-    NAV,
+    ALPHA,
+    EXTEND,
+    SYMBOL,
+    NUMBER,
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[ALPHAS] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T,    KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,    KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                          KC_1, KC_BSPC, KC_SPC, KC_4
-    ),
-    [NAV] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                             KC_NO, KC_NO, KC_NO, KC_NO
+	[ALPHA] = LAYOUT(
+        KC_Q, KC_W, KC_E, KC_R,   KC_T,    KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
+        KC_A, KC_S, KC_D, KC_F,   KC_G,    KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN,
+        KC_Z, KC_X, KC_C, KC_V,   KC_B,    KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
+                          KC_TAB, KC_LSFT, KC_SPC, KC_ENT
     ),
 };
 // clang-format on
