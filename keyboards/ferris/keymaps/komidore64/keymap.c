121514af825c062ed2483e4a7712be71666d979e
Adam Price <komidore64@gmail.com>
2023-08-31 16:50:05 -0400

    wip
    add all layer skeletons as described by miryoku

--- keyboards/ferris/keymaps/komidore64/keymap.c
+++ keyboards/ferris/keymaps/komidore64/keymap.c
@@ -8,36 +8,58 @@
 
 enum layers {
     ALPHA,
-    EXTEND,
-    SYMBOL,
-    NUMBER,
+    FUNC,
+    MEDIA,
+    MOUSE,
+    NAV,
+    NUM,
+    SYM,
 };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ALPHA] = LAYOUT(
-        KC_Q,         KC_W,         KC_E,        KC_R,        KC_T,                /* */ KC_Y,   KC_U,        KC_I,        KC_O,         KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                /* */ KC_H,   SFT_T(KC_J), CTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN),
-        KC_Z,         KC_X,         KC_C,        KC_V,        KC_B,                /* */ KC_N,   KC_M,        KC_COMM,     KC_DOT,       KC_SLSH,
-                                                 KC_NO,       LT(EXTEND, KC_BSPC), /* */ KC_SPC, MO(SYMBOL)
+        KC_Q,         KC_W,         KC_E,        KC_R,        KC_T,                   /* */ KC_Y,            KC_U,            KC_I,        KC_O,         KC_P,
+        LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                   /* */ KC_H,            SFT_T(KC_J),     CTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN),
+        KC_Z,         KC_X,         KC_C,        KC_V,        KC_B,                   /* */ KC_N,            KC_M,            KC_COMM,     KC_DOT,       KC_SLSH,
+                                                 LT(MOUSE, KC_ESC), LT(NAV, KC_BSPC), /* */ LT(SYM, KC_SPC), LT(NUM, KC_TAB)
+                                                 /*             media             */        /*          function          */
     ),
-    [EXTEND] = LAYOUT(
-        KC_NO,   KC_NO,   KC_KB_VOLUME_UP, KC_KB_VOLUME_DOWN, KC_NO, /* */ KC_HOME, KC_PGDN,    KC_PGUP, KC_END,  KC_NO,
-        KC_LGUI, KC_LALT, KC_LCTL,         KC_LSFT,           KC_NO, /* */ KC_LEFT, KC_DOWN,    KC_UP,   KC_RGHT, KC_NO,
-        KC_ESC,  KC_NO,   KC_NO,           KC_TAB,            KC_NO, /* */ KC_NO,   KC_ENT,     KC_NO,   KC_NO,   KC_DEL,
-                                           KC_TRNS,           KC_NO, /* */ KC_ENT,  MO(NUMBER)
+    [FUNC] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
     ),
-    [SYMBOL] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-                             MO(NUMBER), KC_NO, /* */ KC_NO, KC_TRNS
+    [MEDIA] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
     ),
-    [NUMBER] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
-                             KC_TRNS, KC_NO, /* */ KC_NO, KC_TRNS
+    [MOUSE] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
+    ),
+    [NAV] = LAYOUT(
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,   /* */ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_TAB,  KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL,
+                                   KC_NO,   KC_TRNS, /* */ KC_ENT,  KC_NO
+    ),
+    [NUM] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
+    ),
+    [SYM] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
     ),
 };
 // clang-format on
