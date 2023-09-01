431c7b459115aabc91e340754fced64d8b334609
Adam Price <komidore64@gmail.com>
2023-09-01 14:39:35 -0400

    wip
    COMBOS!

--- keyboards/ferris/keymaps/komidore64/keymap.c
+++ keyboards/ferris/keymaps/komidore64/keymap.c
@@ -2,13 +2,7 @@
 
 // #include "version.h"
 
-// NOTES:
-// - so far, i'm not sure i'm digging home row mods. seems to work fine for my
-// stronger fingers (index, and middle), but weaker fingers (ring, pinky) were
-// getting some mod key misfires when typing. will have to keep any eye on it.
-
 // inspirations:
-//   - https://keymapdb.com/keymaps/kkga/
 //   - miryoku
 
 enum layers {
@@ -21,6 +15,14 @@ enum layers {
     SYM,
 };
 
+const uint16_t PROGMEM function_layer[] = { LT(SYM, KC_SPC), LT(NUM, KC_TAB), COMBO_END };
+const uint16_t PROGMEM media_layer[] = { LT(MOUSE, KC_ESC), LT(NAV, KC_BSPC), COMBO_END };
+
+combo_t key_combos[] = {
+    COMBO(function_layer, MO(FUNC)),
+    COMBO(media_layer, MO(MEDIA)),
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ALPHA] = LAYOUT(
@@ -28,25 +30,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                   /* */ KC_H,            SFT_T(KC_J),     CTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT),
         KC_Z,         KC_X,         KC_C,        KC_V,        KC_B,                   /* */ KC_N,            KC_M,            KC_COMM,     KC_DOT,       KC_SLSH,
                                                  LT(MOUSE, KC_ESC), LT(NAV, KC_BSPC), /* */ LT(SYM, KC_SPC), LT(NUM, KC_TAB)
-                                                 /*        hold media             */        /*     hold function          */
     ),
     [FUNC] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
+        KC_F12, KC_F7, KC_F8, KC_F9,  KC_PSCR, /* */ KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,
+        KC_F11, KC_F4, KC_F5, KC_F6,  KC_SCRL, /* */ KC_NO,   KC_NO, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_F10, KC_F1, KC_F2, KC_F3,  KC_PAUS, /* */ KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,
+                              KC_APP, KC_NO,   /* */ KC_TRNS, KC_TRNS
     ),
     [MEDIA] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                             KC_TRNS, KC_TRNS, /* */ KC_NO, KC_NO
     ),
     [MOUSE] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                             KC_NO, KC_NO, /* */ KC_NO, KC_NO
+                             KC_TRNS, KC_NO, /* */ KC_NO, KC_NO
     ),
     [NAV] = LAYOUT(
         KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
