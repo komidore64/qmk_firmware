d42252abe8cab18d63a7de39359dde00b08c3f9a
Adam Price <komidore64@gmail.com>
2023-09-01 09:08:22 -0400

    wip

--- keyboards/ferris/keymaps/komidore64/config.h
+++ keyboards/ferris/keymaps/komidore64/config.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #undef EE_HANDS
-#undef MASTER_RIGHT
 #undef MASTER_LEFT
+#undef MASTER_RIGHT
+#undef TAPPING_TERM
 
 #define MASTER_LEFT
 #define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 350 // milliseconds
--- keyboards/ferris/keymaps/komidore64/keymap.c
+++ keyboards/ferris/keymaps/komidore64/keymap.c
@@ -25,7 +25,7 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[ALPHA] = LAYOUT(
         KC_Q,         KC_W,         KC_E,        KC_R,        KC_T,                   /* */ KC_Y,            KC_U,            KC_I,        KC_O,         KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                   /* */ KC_H,            SFT_T(KC_J),     CTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN),
+        LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,                   /* */ KC_H,            SFT_T(KC_J),     CTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT),
         KC_Z,         KC_X,         KC_C,        KC_V,        KC_B,                   /* */ KC_N,            KC_M,            KC_COMM,     KC_DOT,       KC_SLSH,
                                                  LT(MOUSE, KC_ESC), LT(NAV, KC_BSPC), /* */ LT(SYM, KC_SPC), LT(NUM, KC_TAB)
                                                  /*        hold media             */        /*     hold function          */
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
         KC_LGUI, KC_LALT,  KC_LCTL, KC_LSFT, KC_NO,   /* */ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, CW_TOGG,
         KC_NO,   KC_CUT,   KC_COPY, KC_PSTE, KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_DEL,
-                                    KC_ESC,  KC_TRNS, /* */ KC_ENT,  KC_TAB
+                                    KC_NO,   KC_TRNS, /* */ KC_ENT,  KC_NO
     ),
     [NUM] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -61,10 +61,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_NO, KC_NO, /* */ KC_NO, KC_NO
     ),
     [SYM] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
-                             KC_NO, KC_NO, /* */ KC_TRNS, KC_NO
+        KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, /* */ KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,
+        KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, /* */ KC_NO,   KC_NO, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, /* */ KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,
+                                   KC_UNDS, KC_RPRN, /* */ KC_TRNS, KC_NO
     ),
 };
 // clang-format on
