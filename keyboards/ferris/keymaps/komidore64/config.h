fc91325cb1071e7bf64fc9698081e33472f89fa8
Adam Price <komidore64@gmail.com>
2023-09-03 17:26:09 -0400

    wip

--- keyboards/ferris/keymaps/komidore64/config.h
+++ keyboards/ferris/keymaps/komidore64/config.h
@@ -1,13 +1,23 @@
 #pragma once
 
+// handedness
 #undef EE_HANDS
 #undef MASTER_LEFT
 #undef MASTER_RIGHT
-#undef TAPPING_TERM
-
 #define MASTER_LEFT
-#define MOUSEKEY_INTERAL 33
+
+// mouse
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_INTERVAL 33
 #define MOUSEKEY_MAX_SPEED 7
+
+// the rest
+// #undef COMBO_TERM
+#undef PERMISSIVE_HOLD
+#undef QUICK_TAP_TERM
+#undef TAPPING_TERM
+// #define COMBO_TERM 50
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 400
